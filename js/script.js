b9b346401b0666b518916cb52eed61e219a6099e
KingLegend123 <mastershane21@gmail.com>
2018-03-13 22:15:50 +0000

    new project

--- js/script.js
+++ js/script.js
@@ -2,9 +2,31 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
-$(document).ready(function(){
-  
-  
-  
-  
-});
+$(document).ready(function() {
+
+            function giphyURLWithSearchTerm(searchTerm) {
+
+                var newURL = "https://api.giphy.com/v1/stickers/search?q=" + searchTerm + "&api_key=dc6zaTOxFJmzC";
+                return newURL;
+            }
+
+            function giphyURLWithSearchTerm(searchTerm) {
+
+                var newURL = "https://api.giphy.com/v1/stickers/search?q=" + searchTerm + "&api_key=dc6zaTOxFJmzC";
+                return newURL;
+            }
+
+            function callGiphyAPIWithSearchTerm(searchTerm) {
+                $.ajax({
+                    url: giphyURLWithSearchTerm(searchTerm),
+                    method: "GET",
+                    success: function(response) {
+                        var url = response.data[0].images.original.url;
+                        appendImageToBody(url);
+                    },
+                });
+            }
+            $("button").click(function() {
+                var searchTerm = $("input").val();
+                callGiphyAPIWithSearchTerm(searchTerm);
+            });
